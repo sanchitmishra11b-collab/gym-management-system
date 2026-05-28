aec29957edf910124ef0299a2469e9d3bdeee02a
Sanchit Mishra <sanchitmishra11b@gmail.com>
2026-05-28 13:33:09 +0530

    CSS ERROR

--- djangogym/settings.py
+++ djangogym/settings.py
@@ -89,7 +89,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'djangogym1', 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+WHITENOISE_USE_FINDERS = True
 
 # ── MEDIA ──
 MEDIA_URL = '/media/'
--- djangogym1/context_processors.py
+++
@@ -1,7 +0,0 @@
-from .models import AdminProfile
-
-def pending_count(request):
-    if request.user.is_authenticated and request.user.is_superuser:
-        count = AdminProfile.objects.filter(is_approved=False).count()
-        return {'pending_count': count}
-    return {'pending_count': 0}
